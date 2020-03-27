55c630ab69d644f86b388d13ab3d55d5c1632100
wh0212 <2749143738@qq.com>
2020-03-27 21:35:10 +0800

    teach

--- src/components/Tabbar.vue
+++ src/components/Tabbar.vue
@@ -4,7 +4,7 @@
     <van-tabbar-item to="/course" icon=" iconfont icon-kecheng">课程</van-tabbar-item>
     <van-tabbar-item to="/record" icon=" iconfont icon-dianji">约课记录</van-tabbar-item>
     <van-tabbar-item to="/practice" icon=" iconfont icon-lianxi">练习</van-tabbar-item>
-    <van-tabbar-item to="/login" icon=" iconfont icon-yonghu">我的</van-tabbar-item>
+    <van-tabbar-item to="/mine" icon=" iconfont icon-yonghu">我的</van-tabbar-item>
   </van-tabbar>
 </template>
 <script>
--- src/views/index/swiperId.vue
+++ src/views/index/swiperId.vue
@@ -27,11 +27,24 @@
       <div class="actives">
         <p>活动:</p>
         <div class="vip">优惠</div>
-        <p>领取优惠券最多可减 &nbsp;<i class="iconfont icon-jinbi"></i>&nbsp;10</p>
+        <p>
+          领取优惠券最多可减 &nbsp;
+          <i class="iconfont icon-jinbi"></i>&nbsp;10
+        </p>
         <p class="reveive">领取></p>
       </div>
       <div class="teach">
-          <p class="title">教学团队</p>
+        <p class="title">教学团队</p>
+        <div class="team_item">
+          <ul>
+            <li v-for="(item,index) in data.teachers" :key="index">
+              <div class="avatar">
+                <img class="image" :src="item.avatar" alt />
+              </div>
+              <div class="txt">{{item.teacher_name}}</div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -77,16 +90,51 @@ export default {
 
 
 <style lang="scss" scoped>
-.teach{
-    width: 100%;
-    height: 145px;
-    background: #fff;
-    margin-top: 15px;
-    .title{
-        padding-left: 10px;
-        padding-top: 10px;
-        font-size: 16px;
-    };
+.teach {
+  width: 100%;
+  height: 145px;
+  background: #fff;
+  margin-top: 15px;
+  .title {
+    padding-left: 10px;
+    padding-top: 10px;
+    font-size: 16px;
+  }
+  .team_item {
+    padding-left: 10px;
+    overflow: hidden;
+    ul {
+      display: flex;
+      align-items: center;
+      li {
+        width: 70px;
+        float: left;
+        margin-right: 10px;
+        .avatar {
+          width: 40px;
+          height: 40px;
+          border-radius: 100%;
+          margin: 0 auto;
+        }
+        img {
+          width: 40px;
+          height: 40px;
+          border-radius: 100%;
+        }
+        .txt {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-line-clamp: 1;
+          -webkit-box-orient: vertical;
+        }
+      }
+    }
+    .image {
+      width: 100%;
+      border-radius: 100%;
+    }
+  }
 }
 .icon-jinbi {
   color: #e9bf17;
@@ -99,17 +147,17 @@ export default {
   margin-top: 15px;
   display: flex;
   align-items: center;
-  .vip{
-      width: 38px;
-      height: 19px;
-      text-align: center;
-      border-radius: 5px;
-      overflow: hidden;
-      background-image: url("../../assets/images/bg-vip-board.png");
-      background-size: 100% 100%;
+  .vip {
+    width: 38px;
+    height: 19px;
+    text-align: center;
+    border-radius: 5px;
+    overflow: hidden;
+    background-image: url("../../assets/images/bg-vip-board.png");
+    background-size: 100% 100%;
   }
-  .reveive{
-     margin-left: 70px;
+  .reveive {
+    margin-left: 70px;
   }
   p {
     padding-left: 10px;
