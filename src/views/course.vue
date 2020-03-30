21252274b0ce8f4a754dd0a7ad7f5b1964d967be
wh0212 <2749143738@qq.com>
2020-03-30 18:18:16 +0800

    youhua

--- src/views/course.vue
+++ src/views/course.vue
@@ -9,26 +9,35 @@
         <HandMenu @reset="resetfun" @attr="attrId" @course="courseda" @sreen="sreenda" />
       </div>
     </div>
-    <div class="content">
+    <van-list
+      finished-text="没有更多了"
+      v-model="loading"
+      :finished="finished"
+      @load="onLoad"
+      class="content"
+    >
       <Courseitem :data="curseData.list" />
-    </div>
+    </van-list>
     <div class="bottom"></div>
   </div>
 </template>
 
 <script>
-import { Icon } from "vant";
+import { Icon, List } from "vant";
 import Courseitem from "../components/course/course_item";
 import HandMenu from "../components/course/handMenu";
 import { coursebasis } from "../request/http";
 export default {
   components: {
     [Icon.name]: Icon,
+    [List.name]: List,
     Courseitem,
     HandMenu
   },
   data() {
     return {
+      loading: false,
+      finished: true,
       curseData: {},
       params: {
         page: 1,
@@ -40,47 +49,65 @@ export default {
   },
   mounted() {
     coursebasis().then(res => {
-      // console.log(res.data.data);
       this.curseData = res.data.data;
+      this.finished = false;
     });
-    this.$nextTick(() => {
-      var bady = document.body;
-      // 获取滚动条的dom
-      bady.onscroll = () => {
-        var scrollTop =document.documentElement.scrollTop || document.body.scrollTop;
+    // this.$nextTick(() => {
+    //   var bady = document.body;
+    //   // 获取滚动条的dom
+    //   bady.onscroll = () => {
+    //     var scrollTop =
+    //       document.documentElement.scrollTop || document.body.scrollTop;
 
-        var windowHeight =document.documentElement.clientHeight || document.body.clientHeight;
+    //     var windowHeight =
+    //       document.documentElement.clientHeight || document.body.clientHeight;
 
-        var scrollHeight =document.documentElement.scrollHeight || document.body.scrollHeight;
+    //     var scrollHeight =
+    //       document.documentElement.scrollHeight || document.body.scrollHeight;
 
-        //  console.log("距顶部"+scrollTop+"可视区高度"+windowHeight+"滚动条总高度"+scrollHeight);
-        if (scrollTop + windowHeight >= scrollHeight - 10) {
-          // 把距离顶部的距离加上可视区域的高度 等于或者大于滚动条的总高度就是到达底部
-          console.log("触发了");
-          this.params.page++;
-          coursebasis(this.params).then(res => {
-            const { current_page, list, last_page } = res.data.data;
-            this.curseData.last_page = last_page;
-            this.curseData.current_page = current_page;
-            this.curseData.list = [...this.curseData.list, ...list];
-          });
-        }
-      };
-    });
+    //     //  console.log("距顶部"+scrollTop+"可视区高度"+windowHeight+"滚动条总高度"+scrollHeight);
+    //     if (scrollTop + windowHeight >= scrollHeight - 10) {
+    //       // 把距离顶部的距离加上可视区域的高度 等于或者大于滚动条的总高度就是到达底部
+    //       console.log("触发了");
+    //       this.params.page++;
+    //       coursebasis(this.params).then(res => {
+    //         const { current_page, list, last_page } = res.data.data;
+    //         this.curseData.last_page = last_page;
+    //         this.curseData.current_page = current_page;
+    //         this.curseData.list = [...this.curseData.list, ...list];
+    //       });
+    //     }
+    //   };
+    // });
   },
   methods: {
-    search(){
-      this.$router.push("/search")
+    onLoad() {
+      this.params.page++;
+      coursebasis(this.params).then(res => {
+        this.loading = false;
+        const { current_page, list, last_page } = res.data.data;
+        if (list.length == 0 && this.page != 1) {
+          this.finished = true;
+          this.onFinished && this.onFinished();
+          return;
+        }
+        this.curseData.last_page = last_page;
+        this.curseData.current_page = current_page;
+        this.curseData.list = [...this.curseData.list, ...list];
+      });
+      // this.finished= true
     },
-    resetfun(){
-      this.beg()
+    search() {
+      this.$router.push("/search");
+    },
+    resetfun() {
+      this.beg();
     },
     courseda(v) {
       this.params.order_by = v;
       this.beg(this.params);
     },
     sreenda(v) {
- 
       this.params.course_type = v;
       this.beg(this.params);
     },
@@ -100,7 +127,9 @@ export default {
 
 <style lang="scss" scoped>
 @import "../assets/CSS/index.scss";
-
+.bottom {
+  height: 40px;
+}
 .icon {
   float: right;
   line-height: 1rem;
--- src/views/course/Search.vue
+++ src/views/course/Search.vue
@@ -23,7 +23,7 @@
         </div>
       </div>
       <div v-else>
-        <van-list v-model="loading" :finished="finished" @load="onLoad">
+        <van-list  finished-text="没有更多了" v-model="loading" :finished="finished" @load="onLoad">
           <CourseSearitem :data="classList" v-if="classList.length" />
         </van-list>
         <div class="empty" v-if="emptybool">
@@ -117,6 +117,7 @@ export default {
       }
     },
     onLoad() {
+
       this.page++;
       this.beg()
     }
